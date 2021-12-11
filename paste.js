2c8f22982115bfc9fb45e913dcd3b4f42e1948e1
Shadowz <85980041+Shadowz0001@users.noreply.github.com>
2021-12-11 20:43:48 +0530

    Create paste.js
    mongoose schema

---
+++ paste.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const pasteSchema = new mongoose.Schema({
+    code: {type: String, required: true},
+    identifier: {type: String, required: true}
+})
+
+const pasteModel = mongoose.model("pasteModel", pasteSchema);
+
+module.exports = pasteModel
