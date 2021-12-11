03ed3b98eb079df684b434d70beaf1f362d61fc2
Shadowz <85980041+Shadowz0001@users.noreply.github.com>
2021-12-11 20:44:43 +0530

    mongoose schema

--- paste.js
+++ paste.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const pasteSchema = new mongoose.Schema({
     code: {type: String, required: true},
     identifier: {type: String, required: true}
-})
+});
 
 const pasteModel = mongoose.model("pasteModel", pasteSchema);
 
